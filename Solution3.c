86a939b824e4954555070ea119d5e7868908ab43
Neha Pramanik <78261275+NehaPramanik@users.noreply.github.com>
2021-11-19 16:41:45 +0530

    Update Solution3.c

--- Solution3.c
+++ Solution3.c
@@ -1,73 +1,35 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include <limits.h>
+
+char primesArray[INT_MAX] = {0};
 
 
+void sieveOfEratosthenes(int n) {
 
-void insertionSort(int n, int a[]) {
-  int key, j;
-  for (int i = 1; i < n; i++)
-  {
-    key = a[i];
-    j = i - 1;
-    while (j >= 0 && a[j] > key)
-    {
-        a[j + 1] = a[j];
-        j = j - 1;
+  for(int i = 2; i*i<=n; i++) {
+    if(primesArray[i] == 0) {
+      for(int j = 2; j*i<=n; j++)
+        primesArray[i*j] = 1;
     }
-    a[j + 1] = key;
   }
+
 }
 
-int main() {
+void outputPrimes(int n) {
+  for(int i = 2; i<=n; i++) {
+    if(primesArray[i] == 0)
+      printf("%d ", i);
+  }
+  printf("\n");
+}
+
+int main() {  
 
   int n;
-  printf("Enter size of array: ");
   scanf("%d", &n);
-  
-   int arr[n];
-  
-  
 
-    for(int i = 0; i<n; i++){
-      arr[i] = i;}
-    
-    clock_t start, end;
-     start = clock();
-
-    insertionSort(n, arr);
-
-     end = clock();
-    
-    printf("Total time taken by insertion sort for Best Case is : %f\n", (double)(end - start)/(double)CLOCKS_PER_SEC);
-    
-    for (int i = 0; i < n; i++) {
-			 int x = rand() % n + 1;
-			arr[i] = x;
-        
-    }
-    
-     start = clock();
-
-    insertionSort(n, arr);
-
-     end = clock();
-    
-    printf("Total time taken by insertion sort for Average Case is : %f\n", (double)(end - start)/(double)CLOCKS_PER_SEC);
-    
-    
-    for(int i = 0; i<n; i++){
-      arr[i] = n-i-1;
-        
-    }
-     start = clock();
-
-    insertionSort(n, arr);
-
-     end = clock();
-    
-    printf("Total time taken by insertion sort for Worst Case is : %f\n\n", (double)(end - start)/(double)CLOCKS_PER_SEC);
-    
+  sieveOfEratosthenes(n);
+  outputPrimes(n);
 
   return 0;
 }
